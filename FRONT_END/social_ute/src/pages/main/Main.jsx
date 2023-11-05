a572d23a543e3c6de478287c5dea8c0323273f8e
edtech.intern <edtech.intern@vus-etsc.edu.vn>
2023-11-05 21:44:10 +0700

    Login Fix Redux

--- FRONT_END/social_ute/src/pages/main/Main.jsx
+++ FRONT_END/social_ute/src/pages/main/Main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { selectCurrentToken } from '../../app/slice/auth/auth.slice'
 
 const Public = () => {
+
+    const token = useSelector(selectCurrentToken)
+
     return (
         <div>
             <header>
@@ -15,7 +20,7 @@ const Public = () => {
                 </p>
             </main>
             <footer>
-                <Link to='./login'>Login</Link>
+                {token ? <Link to='./welcome'>welcome</Link> : <Link to='./login'>Login</Link>}
             </footer>
         </div>
     )
--- FRONT_END/social_ute/src/pages/welcome/Welcome.jsx
+++ FRONT_END/social_ute/src/pages/welcome/Welcome.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { selectCurrentUser } from '../../app/slice/auth/auth.slice'
+import { useCookies } from 'react-cookie'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { logOut, selectCurrentToken, selectCurrentUser } from '../../app/slice/auth/auth.slice'
 
 const Welcome = () => {
+    const token = useSelector(selectCurrentToken)
     const user = useSelector(selectCurrentUser)
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const [cookies, removeCookie] = useCookies(['access-token'])
     const welcome = user ? `Welcome ${user}!` : `Welcome `
+
+    const handleLogout = () => {
+        dispatch(logOut())
+        removeCookie(['access-token'])
+        navigate('/')
+    }
 
     return (
         <section >
+            <h1>{token}</h1>
             <h1>{welcome}</h1>
+            <button onClick={handleLogout}>
+                Logout
+            </button>
         </section>
     )
 }
