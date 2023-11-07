2a0f46834615b58bebdb66bc2e685d15079f6a35
edtech.intern <edtech.intern@vus-etsc.edu.vn>
2023-11-07 18:00:38 +0700

    [DOAN-15][UI Form Login]: #added form login UI

--- FRONT_END/social_ute/tailwind.config.js
+++ FRONT_END/social_ute/tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const { nextui } = require("@nextui-org/react");
 
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      zIndex: {
+        100: "100",
+        1000: "1000",
+      },
+      fontFamily: {
+        merriweather: ["Merriweather", "serif"],
+      },
+      backgroundImage: {
+        bg_hcmute: "url('./assets/images/bg-hcmute-02.jpg')",
+      },
+    },
+  },
+  plugins: [nextui()],
+};
