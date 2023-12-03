3372a7c26192950b091bdd592e95b61ba115d613
hungphu2151 <20110540@student.hcmute.edu.vn>
2023-12-03 12:05:18 +0700

    update logic statisticsController

--- BACK_END/controllers/statisticsController.js
+++ BACK_END/controllers/statisticsController.js
@@ -8,13 +8,14 @@ const User = require('../models/User')
 
 exports.getStatistics = (async (req, res) => {
     try {
-        const user = await User.findOne({ user_id:req.params.id })
+        const user = await User.findOne({ _id:req.params.id }).select('_id')
         if(!user){
             return res.status(400).json({
                 success: false,
                 message: 'Không tìm thấy người dùng', 
             });
         }
+
         const count_posts = await Post.countDocuments({ user_id:req.params.id });
 
         const user_follow = await Follow.findOne({ user_id:req.params.id });
@@ -22,10 +23,11 @@ exports.getStatistics = (async (req, res) => {
         const count_followings = user_follow ? user_follow.following_user_id.length : 0;
 
         const user_friend = await Friend.findOne({ user_id:req.params.id });
-        const count_friends = user_friend ? user.friend_id.length : 0;
+        const count_friends = user_friend ? user_friend.friend_id.length : 0;
 
         res.status(200).json({
             success: true,
+            user,
             message: 'Thống kê:',
             count_posts,
             count_followers,
