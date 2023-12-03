a4d3697bd2290cf78d65a0bd4e354523607ed466
hungphu2151 <20110540@student.hcmute.edu.vn>
2023-12-03 11:57:15 +0700

    updata logic statisticsController

--- BACK_END/controllers/statisticsController.js
+++ BACK_END/controllers/statisticsController.js
@@ -2,26 +2,32 @@ const Post = require('../models/Post')
 const Story = require('../models/Story')
 const Follow = require('../models/Follow')
 const Friend = require('../models/Friend')
+const User = require('../models/User')
 
 //GET /statistics/:id
 
 exports.getStatistics = (async (req, res) => {
     try {
+        const user = await User.findOne({ user_id:req.params.id })
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: 'Không tìm thấy người dùng', 
+            });
+        }
         const count_posts = await Post.countDocuments({ user_id:req.params.id });
-        // const count_stories = await Story.countDocuments({ user_id:req.params.id });
 
         const user_follow = await Follow.findOne({ user_id:req.params.id });
         const count_followers = user_follow ? user_follow.follower_user_id.length : 0;
         const count_followings = user_follow ? user_follow.following_user_id.length : 0;
 
-        const user = await Friend.findOne({ user_id:req.params.id });
-        const count_friends = user ? user.friend_id.length : 0;
+        const user_friend = await Friend.findOne({ user_id:req.params.id });
+        const count_friends = user_friend ? user.friend_id.length : 0;
 
         res.status(200).json({
             success: true,
             message: 'Thống kê:',
             count_posts,
-            // count_stories,
             count_followers,
             count_followings,
             count_friends,
