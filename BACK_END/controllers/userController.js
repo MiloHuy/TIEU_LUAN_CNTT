ea06ff9691c28dd13e3766d0206c5fe1ae45f3e0
hungphu2151 <20110540@student.hcmute.edu.vn>
2024-07-15 21:12:57 +0700

    update userController

--- BACK_END/controllers/userController.js
+++ BACK_END/controllers/userController.js
@@ -1,47 +1,50 @@
-const User = require('../models/User')
-const Friend = require('../models/Friend');
-const { UserAPIFeatures } = require('../utils/APIFeatures');
-const { query } = require('express');
-const Post = require('../models/Post');
-const Post_liked = require('../models/Post_liked');
-const Addfriend = require('../models/Addfriend');
-const Follow = require('../models/Follow');
+const User = require("../models/User");
+const Friend = require("../models/Friend");
+const { UserAPIFeatures } = require("../utils/APIFeatures");
+const { query } = require("express");
+const Post = require("../models/Post");
+const Post_liked = require("../models/Post_liked");
+const Addfriend = require("../models/Addfriend");
+const Follow = require("../models/Follow");
 
 //GET /info/:id
-exports.getInfo = (async (req, res) => {
+exports.getInfo = async (req, res) => {
     try {
-        if(req.params.id==req.user._id){
+        if (req.params.id == req.user._id) {
             return res.status(400).json({
                 success: false,
                 code: 4000,
-                message:'Phải dùng id của người khác, không được dùng id của bản thân.',
+                message:
+                    "Phải dùng id của người khác, không được dùng id của bản thân.",
             });
         }
-        const user = await User.findOne({ _id: req.params.id }).select('first_name last_name avatar.url id department');
-        if(!user){
+        const user = await User.findOne({ _id: req.params.id }).select(
+            "first_name last_name avatar.url id department"
+        );
+        if (!user) {
             return res.status(404).json({
                 success: false,
                 code: 4001,
-                message: 'Không tìm thấy người dùng.', 
+                message: "Không tìm thấy người dùng.",
             });
         }
         const check_friend = await Friend.findOne({
             user_id: req.user._id,
             friend_id: req.params.id,
-        })
+        });
         const friend = check_friend ? true : false;
         const check_following = await Follow.findOne({
             user_id: req.user._id,
             following_user_id: req.params.id,
-        })
+        });
         const following = check_following ? true : false;
-        if(!friend){
+        if (!friend) {
             const check_friend_request = await Addfriend.findOne({
                 user_id: req.params.id,
-                add_user_id: req.user._id
-            })
+                add_user_id: req.user._id,
+            });
             const friend_request = check_friend_request ? true : false;
-            if(friend_request){
+            if (friend_request) {
                 return res.status(200).json({
                     success: true,
                     user,
@@ -52,8 +55,8 @@ exports.getInfo = (async (req, res) => {
             }
             const check_add_friend = await Addfriend.findOne({
                 user_id: req.user._id,
-                add_user_id: req.params.id
-            })
+                add_user_id: req.params.id,
+            });
             const add_friend = check_add_friend ? true : false;
             return res.status(200).json({
                 success: true,
@@ -73,162 +76,207 @@ exports.getInfo = (async (req, res) => {
         res.status(500).json({
             success: false,
             code: 4002,
-            message: error, 
+            message: error,
         });
     }
-})
+};
 
 //GET /posts/:id
-exports.getPosts = (async (req, res) => {
+exports.getPosts = async (req, res) => {
     try {
+        const { size = 10, page = 1 } = req.query;
+        const skip = size * (page - 1);
         const is_follow = await Follow.findOne({
             user_id: req.user._id,
-            following_user_id: req.params.id
+            following_user_id: req.params.id,
         }).lean();
 
-        if(!is_follow){
-            const posts = await Post.find({ user_id: req.params.id, privacy: 2 })
-            .select('_id post_img.url')
-            .lean()
-            const postsAfferCountLike = await Promise.all(posts.map(async (post) => {
-                const post_like = await Post_liked.findOne({ post_id: post._id });
+        if (!is_follow) {
+            const [posts, totals] = await Promise.all([
+                Post.find({ user_id: req.params.id, privacy: 2 })
+                    .select("_id post_img.url")
+                    .sort({ create_post_time: -1 })
+                    .limit(Number(size))
+                    .skip(skip)
+                    .lean(),
+                Post.countDocuments({ user_id: req.params.id, privacy: 2 }),
+            ]);
+            // const posts = await Post.find({ user_id: req.params.id, privacy: 2 })
+            // .select('_id post_img.url')
+            // .sort({ create_post_time: -1 })
+            // .limit(Number(size))
+            // .skip(skip)
+            // .lean()
+            const postsAfferCountLike = await Promise.all(
+                posts.map(async (post) => {
+                    const post_like = await Post_liked.findOne({
+                        post_id: post._id,
+                    });
+                    const likes = post_like ? post_like.user_id.length : 0;
+
+                    //Làm thêm phần count_cmt
+
+                    return {
+                        ...post,
+                        likes,
+                    };
+                })
+            );
+            return res.status(200).json({
+                success: true,
+                posts: postsAfferCountLike,
+                totals,
+            });
+        }
+        const [posts, totals] = await Promise.all([
+            Post.find({ user_id: req.params.id, privacy: { $in: [1, 2] } })
+                .select("_id post_img.url")
+                .sort({ create_post_time: -1 })
+                .limit(Number(size))
+                .skip(skip)
+                .lean(),
+            Post.countDocuments({ user_id: req.user._id }),
+        ]);
+        // const posts = await Post.find({ user_id : req.params.id, privacy: { $in: [1, 2]} })
+        //     .select('_id post_img.url')
+        //     .sort({ create_post_time: -1 })
+        //     .limit(Number(size))
+        //     .skip(skip)
+        //     .lean()
+
+        const postsAfferCountLike = await Promise.all(
+            posts.map(async (post) => {
+                const post_like = await Post_liked.findOne({
+                    post_id: post._id,
+                });
                 const likes = post_like ? post_like.user_id.length : 0;
-    
+
                 //Làm thêm phần count_cmt
-            
+
                 return {
                     ...post,
                     likes,
                 };
-            }));
-            return res.status(200).json({
-                success: true,
-                posts: postsAfferCountLike
             })
-        }
-
-        const posts = await Post.find({ user_id : req.params.id, privacy: { $in: [1, 2]} })
-            .select('_id post_img.url')
-            .lean()
-
-        const postsAfferCountLike = await Promise.all(posts.map(async (post) => {
-            const post_like = await Post_liked.findOne({ post_id: post._id });
-            const likes = post_like ? post_like.user_id.length : 0;
-
-            //Làm thêm phần count_cmt
-        
-            return {
-                ...post,
-                likes,
-            };
-        }));
+        );
 
         return res.status(200).json({
             success: true,
-            posts: postsAfferCountLike
-        })
-        
+            posts: postsAfferCountLike,
+            totals,
+        });
     } catch (error) {
         res.status(500).json({
             success: false,
             code: 4005,
-            message: error.message, 
+            message: error.message,
         });
     }
-})
+};
 
 //GET /users/admin
-exports.getAll = (async (req, res, next) => {
+exports.getAll = async (req, res, next) => {
     try {
         const { size } = req.query;
-        const userQuery = User.find({ role_id: { $ne: 0 } }).select('avatar.url first_name last_name gmail id department phone_number is_active');
-        
-        const apiFeatures = new UserAPIFeatures(userQuery, req.query)
-        
+        const userQuery = User.find({ role_id: { $ne: 0 } }).select(
+            "avatar.url first_name last_name gmail id department phone_number is_active"
+        );
+
+        const apiFeatures = new UserAPIFeatures(userQuery, req.query);
+
         let allUser = await apiFeatures.query;
         const totals = allUser.length;
 
-        const apiFeaturesPagination = new UserAPIFeatures(User.find(userQuery), req.query)
+        const apiFeaturesPagination = new UserAPIFeatures(
+            User.find(userQuery),
+            req.query
+        )
             .search()
             .pagination(size);
 
         allUser = await apiFeaturesPagination.query;
 
-        const count_user = allUser.length
-        
+        const count_user = allUser.length;
+
         return res.status(200).json({
             success: true,
             count_user,
             totals,
-            all_user: allUser, 
+            all_user: allUser,
         });
     } catch (error) {
         res.status(500).json({
             success: false,
             code: 4006,
-            message: error, 
+            message: error,
         });
     }
-})
+};
 
 //POST /users/admin/:id
-exports.disabled = (async (req, res) => {
+exports.disabled = async (req, res) => {
     try {
-        const user = await User.findOne({ _id: req.params.id }).select('is_active');
-        if(!user){
+        const user = await User.findOne({ _id: req.params.id }).select(
+            "is_active"
+        );
+        if (!user) {
             return res.status(404).json({
                 success: false,
                 code: 4007,
-                message: 'Không tìm thấy người dùng.', 
+                message: "Không tìm thấy người dùng.",
             });
         }
-        is_active = !user.is_active
+        is_active = !user.is_active;
         const user_update = await User.updateOne(
-            { _id: req.params.id},
+            { _id: req.params.id },
             { $set: { is_active: is_active } },
-            { new: true },
-        )
+            { new: true }
+        );
         res.status(200).json({
             success: true,
-            user
+            user,
         });
     } catch (error) {
         res.status(500).json({
             success: false,
             code: 4008,
-            message: err.message 
+            message: err.message,
         });
     }
-})
+};
 
 //GET /users/search
-exports.getAllUser = (async (req, res) => {
+exports.getAllUser = async (req, res) => {
     try {
         const { size } = req.query;
-        const userQuery = User.find({ role_id: { $ne: 0 } }).select('avatar.url first_name last_name department');
-        
-        const apiFeatures = new UserAPIFeatures(userQuery, req.query)
-            .search();
-        
+        const userQuery = User.find({ role_id: { $ne: 0 } }).select(
+            "avatar.url first_name last_name department"
+        );
+
+        const apiFeatures = new UserAPIFeatures(userQuery, req.query).search();
+
         let allUser = await apiFeatures.query;
         const totals = allUser.length;
 
-        const apiFeaturesPagination = new UserAPIFeatures(User.find(userQuery), req.query)
+        const apiFeaturesPagination = new UserAPIFeatures(
+            User.find(userQuery),
+            req.query
+        )
             .search()
             .pagination(size);
 
         allUser = await apiFeaturesPagination.query;
-        
+
         return res.status(200).json({
             success: true,
             totals,
-            allUser, 
+            allUser,
         });
     } catch (error) {
         res.status(500).json({
             success: false,
             code: 4009,
-            message: error, 
+            message: error,
         });
     }
-});
+};
